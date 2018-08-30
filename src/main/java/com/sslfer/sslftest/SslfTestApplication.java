976c3a35ac7f41d11960a39fe684d6a52580f0c9
sslf <sslf_vip@163.com>
2018-08-30 14:21:44 +0800

    Initial commit

---
+++ src/main/java/com/sslfer/sslftest/SslfTestApplication.java
@@ -0,0 +1,12 @@
+package com.sslfer.sslftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslfTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslfTestApplication.class, args);
+    }
+}
